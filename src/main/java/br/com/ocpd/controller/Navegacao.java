30e77b687fe3841c0a70c3d9ae5f9b74890b67a6
Dáurio Filho <daurio@pactosolucoes.com.br>
2020-03-02 13:16:52 -0300

    Conclusão navegação e urls API

--- src/main/java/br/com/ocpd/controller/Navegacao.java
+++ src/main/java/br/com/ocpd/controller/Navegacao.java
@@ -8,8 +8,14 @@ public class Navegacao {
         RECUPERAR_SENHA("recuperar_senha.jsf"),
         DASHBOARD("dashboard.jsf"),
         INDEX("index.jsf"),
-        Z(".jsf"),
-        ZZ("");
+        BAIRRO("bairro.jsf"),
+        CIDADE("cidade.jsf"),
+        DISTRITO("distrito.jsf"),
+        LESAO("lesao.jsf"),
+        LOCAL_ATENDIUMENTO("local_atendimento.jsf"),
+        TIPO_LESAO("tipo_lesao.jsf"),
+        TIPO_LOCAL_ATENDIMENTO("tipo_local_atendimento.jsf"),
+        USUARIOS("usuarios.jsf");
 
         private String pagina;
 
